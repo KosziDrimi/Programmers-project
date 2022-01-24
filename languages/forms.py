937911b7a99faf8ca2c707d618edcae8c9601f79
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2022-01-24 10:05:40 +0100

    models updated

--- languages/forms.py
+++ languages/forms.py
@@ -5,14 +5,10 @@ from .models import Programmer
 class ProgrammerForm(forms.ModelForm):
     class Meta:
         model = Programmer
-        fields = ['name', 'surname', 'email', 'position', 'c_plus_plus_level',
-                  'c_level', 'rust_level', 'python_level', 'java_level']
+        fields = '__all__'
         
 
 class LanguageForm(forms.ModelForm):
     class Meta:
         model = Programmer
-        fields = ['c_plus_plus_level', 'c_level', 'rust_level', 
-                  'python_level', 'java_level']
-        
-        
+        fields = ['c_plus_plus_level', 'c_level', 'rust_level', 'python_level', 'java_level']
--- languages/models.py
+++ languages/models.py
@@ -1,17 +1,17 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 
 class Programmer(models.Model):
-    name = models.CharField(max_length=20) 
-    surname = models.CharField(max_length=20)
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
     email = models.EmailField()
-    position = models.CharField(max_length=30)
-    c_plus_plus_level = models.IntegerField(default=0)
-    c_level = models.IntegerField(default=0)
-    rust_level = models.IntegerField(default=0)
-    python_level = models.IntegerField(default=0)
-    java_level = models.IntegerField(default=0)
+    position = models.CharField(max_length=100)
+    c_plus_plus_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
+    c_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
+    rust_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
+    python_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
+    java_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
 
     def __str__(self):
         return f'<{self.name} {self.surname} - {self.position}>'
-
