c4877c8d3d022af9e7e60a716d61f0d88607c8da
Katarzyna Klimiuk <klimiuk.kasia@gmail.com>
2022-01-25 17:26:56 +0100

    choices added to levels' fields

--- languages/models.py
+++ languages/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from django.core.validators import MaxValueValidator
+
+
+level_choices = [(0, 0), (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6)]
 
 
 class Programmer(models.Model):
@@ -7,11 +9,11 @@ class Programmer(models.Model):
     surname = models.CharField(max_length=100)
     email = models.EmailField()
     position = models.CharField(max_length=100)
-    c_plus_plus_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
-    c_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
-    rust_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
-    python_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
-    java_level = models.PositiveSmallIntegerField(default=0, validators=[MaxValueValidator(10)])
+    c_plus_plus_level = models.IntegerField(default=0, choices=level_choices)
+    c_level = models.IntegerField(default=0, choices=level_choices)
+    rust_level = models.IntegerField(default=0, choices=level_choices)
+    python_level = models.IntegerField(default=0, choices=level_choices)
+    java_level = models.IntegerField(default=0, choices=level_choices)
 
     def __str__(self):
         return f'<{self.name} {self.surname} - {self.position}>'
